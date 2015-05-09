6443b4e690e27f0126adc94684c2beeb549c5eb6
Eric Hopkins <scarecrow3322@gmail.com>
2015-05-08 22:22:58 -0400

    move jQuery before Angular
    Hadn't realize this, but Angular uses a micro version of jQuery, or
    it will if there isn't the full version jQuery in the project. This
    changes the compile order so that jQuery is on top, and Angular will
    make use of it rather than it's micro-version.

--- gulpfile.js
+++ gulpfile.js
@@ -10,8 +10,8 @@ var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
     webserver = require('gulp-webserver'),
     vendorJSFiles = [
-        'bower_components/angular/angular.js',
         'bower_components/jquery/dist/jquery.js',
+        'bower_components/angular/angular.js',
         'bower_components/bootstrap/dist/js/bootstrap.js'
     ],
     vendorCSSFiles = [
